cbdf1f4311486b23152cd0d124100695a8f57bb2
gabimattos <50138800+gabimattos@users.noreply.github.com>
2020-07-27 16:39:35 -0300

    + implementação do ngFor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+class Republica{
+
+  nome: string;
+  cidade: string;
+  preco: string;
+  quartos: number;
+  banheiros: number;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,9 +16,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+    republicas: Republica[];
+
   constructor() { }
 
   ngOnInit() {
+    this.republicas = [{
+        
+        nome: 'Republica da Hannah',
+        cidade: 'Rio de Janeiro',
+        preco: 'R$230',
+        quartos: 3,
+        banheiros: 1,
+    },
+    {
+        
+        nome: 'Republica do Ulrich',
+        cidade: 'São Paulo',
+        preco: 'R$300',
+        quartos: 4,
+        banheiros: 2,
+    },
+    {
+       
+        nome: 'Republica do Mikkel',
+        cidade: 'Guarujá',
+        preco: 'R$800',
+        quartos: 8,
+        banheiros: 4,
+    }]
   }
 
 }
